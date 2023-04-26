fc901f12dccd3a44f54b18cc9ded8f6f4f2b294f
Dinamow <meemoo102039@gmail.com>
2023-04-26 03:46:58 +0200

    first

---
+++ printf_HEX_aux.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * printf_HEX_aux - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int printf_HEX_aux(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+}
