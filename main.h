64e407376746d4d88b1e0121e53ff34c660f2c02
mohamedalaga <malaga.142014@gmail.com>
2023-04-26 04:04:11 +0200

    .

--- main.h
+++ main.h
@@ -1,24 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
 #include <unistd.h>
-
-/**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
- * @f: type pointer to function for the conversion specifier
- *
- /
-
 typedef struct format
 {
     charid;
     int (f)();
-} convert_match;
+}
+convert_match;
 int printf_pointer(va_list val);
 int printf_hex_aux(unsigned long int num);
 int printf_HEX_aux(unsigned int num);
