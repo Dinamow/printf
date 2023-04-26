2846d2f34f28b9a7a4b45e4c8e6cdf5d36295517
Dinamow <meemoo102039@gmail.com>
2023-04-26 03:53:16 +0200

    first

---
+++ printf_exclusive_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * printf_exclusive_string - print exclusuives string.
+ * @val: argumen t.
+ * Return: the length of the string.
+ */
+
+int printf_exclusive_string(va_list val)
+{
+
+}
