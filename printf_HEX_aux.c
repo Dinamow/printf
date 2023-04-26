b926f6f1893d1193a4b06e957d3e023eae554385
mohamedalaga <malaga.142014@gmail.com>
2023-04-26 03:51:15 +0200

    .

--- printf_HEX_aux.c
+++ printf_HEX_aux.c
@@ -19,5 +19,17 @@ int printf_HEX_aux(unsigned int num)
 	}
 	counter++;
 	array = malloc(counter * sizeof(int));
-
+for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
 }
