cf6049b423b208fc21f1608fddb986529a884148
Dinamow <meemoo102039@gmail.com>
2023-04-26 03:55:58 +0200

    first

---
+++ printf_int.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * printf_int - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int printf_int(va_list args)
+{
+
+}
+
+/**
+ * printf_dec - prints decimal
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+
+int printf_dec(va_list args)
+{
+
+}
